4ef44ac4b30f7f499a00c2bc0745f65ef4b721b5
Samjidsheikh-1122 <120444741+Samjidsheikh-1122@users.noreply.github.com>
2022-12-13 15:12:05 +0600

    Add files via upload

---
+++ text-samjid.py
@@ -0,0 +1,76 @@
+name=input("inter your name:")
+age=input("inter your age:")
+fb =input("inter your fb account:")
+instagrame=input("inter your instagrame:")
+
+school=input("inter your school name:")
+cs=input("inter your cs:")
+ 
+ 
+ 
+name="samjid sheikh"
+age="16"
+school="Tamai M.L High school"
+clas="NINE"
+year="2022"
+city="Sirajgonj"
+upzella="Belkhuchi"
+number="01963643516"
+gpa="5.66"
+print("\n\n\n\n\n")
+
+print("\n\n\n\n\n\n\n")
+print("\n \n\n\t!!!!!!!! $  my identity  $!!!!!!!!!\t\t\t")
+print("\n\n\n\n\n\n\n\n\n\n\n")
+
+print("Name:"+name)
+print("Age:"+age)
+print("School:"+school)
+print("Class:"+clas)
+print("Year:"+year)
+print("City:"+city)
+print("upzella:"+upzella)
+print("Number:"+number)
+print("gpa:"+gpa)
+ 
+
+print("\"samjid shekih\"")
+print("\n\n\n")
+
+
+
+red='\33[91m'
+
+white='\33[97m'
+
+blue='\33[94m'
+
+yellow='\33[93m'
+
+green='\033[1;32m'
+cyan='\033[96m'
+
+
+
+logo=red+str("""8🤟🤟🤟🤟8\n\n
+                                  
+8888888888  888888     888     8888 
+888        8888  888   88 88  88 88
+8888888888 888    888  88   888   88   
+      8888 88 88888888 88   888   88      
+      8888 888     888 88         88               
+8888888888 88       888 88        88          
+ 
+ 88888888888  88888888  88888888888
+         888     88     88       888      
+         888     88     88         88
+         888     88     88         888
+         888     88     88         88
+         888     88     88        988
+         888     88     88        88
+   888888899  88888889  888 8888888\n\n\n\n\n\n""")                         
+             
+               
+                  
+print(logo)      
+                
